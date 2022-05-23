d27e494cc21ca96c03ddfdcc05cb0de2481647a1
Vishwakumar Patil <vishwakumarp@gmail.com>
2022-05-23 16:49:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "vpc_name"{
   type = string
+  default = "my-test-vpc"
 }
 
 variable "resource_group" {
   type = string
+  default = "Default"
 }
 
 variable "vpc_tags" {
   type = list(string)
+  default = ["test-tag-1", "test-tag-2"]
 }
 
